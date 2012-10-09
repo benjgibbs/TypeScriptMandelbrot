e9c219c7bcd2911c8b3618e951b599f332869817
Ben Gibbs <benjgibbs@gmail.com>
2012-10-09 07:30:19 +0100

    Add some form of working coloring

--- TypeScriptTutorial/Mandelbrot.js
+++ TypeScriptTutorial/Mandelbrot.js
@@ -1,3 +1,8 @@
+var canvas = document.getElementById("mb_canvas");
+var height = parseInt(canvas.getAttribute("height"));
+var width = parseInt(canvas.getAttribute("width"));
+console.log("Width: " + width + ", Height: " + height);
+var ctxt = canvas.getContext("2d");
 var Limit = (function () {
     function Limit(min, max) {
         this.min = min;
@@ -8,37 +13,33 @@ var Limit = (function () {
 function scale(val, limit) {
     return (limit.max - limit.min) / val;
 }
-var max_iterations = 1000;
-var xLimits = new Limit(-2.5, 1);
-var yLimits = new Limit(-1, 1);
-var canvas = document.getElementById("mb_canvas");
-var height = parseInt(canvas.getAttribute("height"));
-var width = parseInt(canvas.getAttribute("width"));
-console.log("Width: " + width + ", Height: " + height);
-var ctxt = canvas.getContext("2d");
-var xscale = scale(width, xLimits);
-var yscale = scale(height, yLimits);
-for(var r = 0; r < height; r++) {
-    var y0 = yLimits.min + yscale * r;
-    for(var c = 0; c < width; c++) {
-        var x0 = xLimits.min + xscale * c;
-        var x = 0;
-        var y = 0;
-        var iteration = 0;
-        while(x * x + y * y < 2 * 2 && iteration < max_iterations) {
-            var xtemp = x * x - y * y + x0;
-            y = 2 * x * y + y0;
-            x = xtemp;
-            iteration++;
+function draw(xLimits, yLimits) {
+    var max_iterations = 1000;
+    var xscale = scale(width, xLimits);
+    var yscale = scale(height, yLimits);
+    for(var r = 0; r < height; r++) {
+        var y0 = yLimits.min + yscale * r;
+        for(var c = 0; c < width; c++) {
+            var x0 = xLimits.min + xscale * c;
+            var x = 0;
+            var y = 0;
+            var iteration = 0;
+            while(x * x + y * y < 2 * 2 && iteration < max_iterations) {
+                var xtemp = x * x - y * y + x0;
+                y = 2 * x * y + y0;
+                x = xtemp;
+                iteration++;
+            }
+            ctxt.beginPath();
+            var colorScale = 65535 / max_iterations;
+            var color = "#" + Math.floor(iteration * colorScale).toString(16) + "FF";
+            ctxt.fillStyle = color;
+            ctxt.rect(c, r, 1, 1);
+            ctxt.fill();
+            ctxt.closePath();
         }
-        ctxt.beginPath();
-        if(iteration == max_iterations) {
-            ctxt.fillStyle = '#FFFFFF';
-        } else {
-            ctxt.fillStyle = '#000000';
-        }
-        ctxt.rect(c, r, 1, 1);
-        ctxt.fill();
-        ctxt.closePath();
     }
 }
+var xLimits = new Limit(-2.5, 1);
+var yLimits = new Limit(-1, 1);
+draw(xLimits, yLimits);
